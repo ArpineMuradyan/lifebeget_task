cb8097fe2267e0268d9cbe5ab16d9c152c716260
ArpineMuradyan <88828449+ArpineMuradyan@users.noreply.github.com>
2022-07-08 20:33:49 +0400

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,18 +9,14 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-{{--    <script src="{{ asset('js/app.js') }}" defer></script>--}}
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-{{--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">--}}
-{{--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">--}}
+    
+    <!--jQuery-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
@@ -34,14 +30,8 @@
                     @include('layouts.sidebar')
                 @endif
             @endauth
-{{--            @if(Route::current()->getPrefix() != '/admin')--}}
-{{--                @include('layouts.front_sidebar')--}}
-{{--            @endif--}}
             @yield('content')
         </div>
-
-{{--        @include('content')--}}
-{{--        <App></App>--}}
     </div>
   @show
 </body>
